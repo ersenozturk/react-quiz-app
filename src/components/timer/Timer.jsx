64114056ab25bd3928839e351f32f78a5ae7ac64
ersen <70ozturkersen@gmail.com>
2022-06-30 21:00:05 +0300

    Timer component done

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Timer.module.css'
 
-const Timer = () => {
+const Timer = ({questionNumber, setStop}) => {
+  const [timeCalc, setTimeCalc] = useState(30)
+
+  useEffect(() => {
+    if (timeCalc === 0) return setStop(true);
+    const interval = setInterval(() => {
+      setTimeCalc((prev) => prev - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timeCalc, setStop]);
+
+  useEffect(() => {
+    setTimeCalc(30)
+  }, [questionNumber])
+  
+  
   return (
     <div className={styles.timerCompDiv}>
-      <p className={styles.timerCompP}>30</p> 
-    </div>
+      <p className={styles.timerCompP}>{timeCalc}</p> 
+    </div> 
   )
 }
 
